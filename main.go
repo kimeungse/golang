fe10571836457edbfdca6cdf757a31a3ea3fdb30
[kimeungse] <[keungse79@gmail.com]>
2022-11-23 21:38:07 +0500

    고루틴

--- main.go
+++ main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/kimeunse/golang/multi"
-	"github.com/kimeunse/golang/single"
+	"time"
 )
 
-func main() {
-	multi.Mul()
-	strr, err := single.Hello("")
-	if err == nil {
-		fmt.Println(strr)
-		fmt.Println("main")
-	} else {
-		fmt.Println(err)
+func f(n int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(n, ":", i)
+		time.Sleep(100)
 	}
+}
+func main() {
+	for i := 0; i < 10; i++ {
+		go f(i)
+	}
+
+	fmt.Println("Waiting...")
+	var input string
+	result, err := fmt.Scanln(&input)
+	if err != nil {
+		fmt.Println("Something wrong...")
+
+	}
+	fmt.Println(result)
 }
